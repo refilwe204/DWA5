163385dbfc830bc596529f1ee382a0127c0023e6
refilwe204 <nqatyelwaangelin@gmail.com>
2023-06-06 13:48:47 +0200

    added comments

--- scripts.js
+++ scripts.js
@@ -1,29 +1,31 @@
-const form = document.querySelector("[data-form]");
-const result = document.querySelector("[data-result]");
+const form = document.querySelector("[data-form]"); // Select the form element
+const result = document.querySelector("[data-result]"); // Select the result element
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+form.addEventListener("submit", (event) => { // Add a submit event listener to the form
+  event.preventDefault(); // Prevent the form from being submitted
+
+  // Get the values from the form inputs using FormData
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
 
-  if (!dividend || !divider) {
+  if (!dividend || !divider) { // Check if both values are present
     result.innerText = "Division not performed. Both values are required in inputs. Try again";
   } else {
     try {
-      const dividendNumber = parseFloat(dividend);
-      const dividerNumber = parseFloat(divider);
+      const dividendNumber = parseFloat(dividend); // Convert dividend to a number
+      const dividerNumber = parseFloat(divider); // Convert divider to a number
 
-      if (isNaN(dividendNumber) || isNaN(dividerNumber) || dividerNumber === 0) {
+      if (isNaN(dividendNumber) || isNaN(dividerNumber) || dividerNumber === 0) { // Check if the numbers are valid
         throw new Error("Invalid number provided");
       }
 
-      const divisionResult = dividendNumber / dividerNumber;
-      if (!Number.isFinite(divisionResult)) {
+      const divisionResult = dividendNumber / dividerNumber; // Perform the division
+      if (!Number.isFinite(divisionResult)) { // Check if the result is a valid number
         throw new Error("Invalid division result");
       }
 
-      const roundedResult = Math.floor(divisionResult);
-      result.innerText = roundedResult;
+      const roundedResult = Math.floor(divisionResult); // Round down the result
+      result.innerText = roundedResult; // Display the result
     } catch (error) {
       result.innerText = "Something critical went wrong. Please reload the page";
       result.style.position = "absolute";
@@ -36,11 +38,12 @@ form.addEventListener("submit", (event) => {
       result.style.display = "flex";
       result.style.justifyContent = "center";
       result.style.alignItems = "center";
-      console.error(error);
-      throw error;
+      console.error(error); // Log the error to the console
+      throw error; // Rethrow the error to propagate it further
     }
   }
 });
+
 
 
 
