d0018a3b329083871e8ddff1c7d65abdf754b16f
refilwe204 <nqatyelwaangelin@gmail.com>
2023-06-06 22:07:28 +0200

    added jsdoc comments

--- scripts.js
+++ scripts.js
@@ -4,30 +4,56 @@ const result = document.querySelector("[data-result]"); // Select the result ele
 // Display "NO calculation performed" at the bottom initially
 result.innerText = "NO calculation performed";
 
-form.addEventListener("submit", (event) => { // Add a submit event listener to the form
+/**
+ * Event listener for the form submission.
+ * @param {Event} event - The submit event object.
+ */
+form.addEventListener("submit", (event) => {
   event.preventDefault(); // Prevent the form from being submitted
 
   // Get the values from the form inputs using FormData
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
 
-  if (!dividend || !divider) { // Check if both values are present
-    result.innerText = "Division not performed. Both values are required in inputs. Try again";
+  if (!dividend || !divider) {
+    // Check if both values are present
+    result.innerText =
+      "Division not performed. Both values are required in inputs. Try again";
   } else {
     try {
-      const dividendNumber = parseFloat(dividend); // Convert dividend to a number
-      const dividerNumber = parseFloat(divider); // Convert divider to a number
+      /**
+       * Convert the dividend to a number.
+       * @typedef {number}
+       */
+      const dividendNumber = parseFloat(dividend);
 
-      if (dividerNumber <3) {
+      /**
+       * Convert the divider to a number.
+       * @typedef {number}
+       */
+      const dividerNumber = parseFloat(divider);
+
+      if (dividerNumber < 3) {
         throw new Error("Division not performed. Invalid number provided. Try again");
       }
 
-      const divisionResult = dividendNumber / dividerNumber; // Perform the division
-      if (!Number.isFinite(divisionResult)) { // Check if the result is a valid number
-        throw new Error("Something critical went wrong, Please reload the page");
+      /**
+       * Perform the division.
+       * @typedef {number}
+       */
+      const divisionResult = dividendNumber / dividerNumber;
+
+      if (!Number.isFinite(divisionResult)) {
+        // Check if the result is a valid number
+        throw new Error("Something critical went wrong. Please reload the page");
       }
 
-      const roundedResult = Math.floor(divisionResult); // Round down the result
+      /**
+       * Round down the result.
+       * @typedef {number}
+       */
+      const roundedResult = Math.floor(divisionResult);
+
       result.innerText = roundedResult.toString(); // Display the result
     } catch (error) {
       result.innerText = error.message;
@@ -35,6 +61,7 @@ form.addEventListener("submit", (event) => { // Add a submit event listener to t
     }
   }
 });
+
 
 
 
